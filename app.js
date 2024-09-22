8a4ea7cc88906753b4df5c29e36547eb1198a3ce
Onkar <onkarmuthe.2016@gmail.com>
2024-09-22 22:11:55 +0530

    changes

---
+++ app.js
@@ -0,0 +1,121 @@
+const express=require("express");
+const app=express();
+const mongoose=require("mongoose");
+const path=require("path");
+const Listing=require("./models/listing.js");
+const methodOverride=require("method-override");
+const ejsMate=require("ejs-mate");
+const wrapAsync=require("./utils/wrapAsync.js");
+const ExpressError=require("./utils/ExpressError.js");
+const {listingSchema}=require("./schema.js");
+
+app.set("view engine","ejs");
+app.set("views",path.join(__dirname,"views"));
+app.use(express.urlencoded({extended:true}));
+app.use(methodOverride("_method"));
+app.engine("ejs",ejsMate);
+app.use(express.static(path.join(__dirname,"/public")));
+
+const mongo_url="mongodb://127.0.0.1:27017/wanderlust";
+
+main()
+.then((res)=>
+{
+    console.log("successful connection");
+})
+.catch(err => console.log(err));
+async function main() {
+  await mongoose.connect(mongo_url);
+
+  // use `await mongoose.connect('mongodb://user:password@127.0.0.1:27017/test');` if your database has auth enabled
+}
+
+
+app.get("/",(req,res)=>
+{
+    res.send("hei i am rooot");
+});
+
+const validateListing=(req,res,next)=>
+{
+    let {error}=listingSchema.validate(req.body);
+        if(error)
+        {
+            let errMsg=error.details.map((el)=>el.message).join(",");
+            throw new ExpressError(400,errMsg);
+        }
+        else{
+            next();
+        }
+
+}
+
+//index route
+app.get("/listings",wrapAsync(async (req,res)=>
+{
+    const allListing=await Listing.find({});
+    res.render("listings/index.ejs",{allListing});
+}))
+
+//new route
+app.get("/listings/new",(req,res)=>
+    {
+        res.render("listings/new.ejs");
+    })
+
+//show route
+app.get("/listings/:id",wrapAsync(async(req,res)=>
+{
+    let{id}=req.params;
+    const listing=await Listing.findById(id);
+    res.render("listings/show.ejs",{listing});
+
+}))
+//create route
+app.post("/listings",validateListing,wrapAsync(async(req,res,next)=>
+{
+        
+        const newlist=new Listing(req.body.listing);
+        await newlist.save();
+        res.redirect("/listings");
+})
+);
+
+//edit route
+app.get("/listings/:id/edit",wrapAsync(async (req,res)=>
+{
+    let {id}=req.params;
+    const listing=await Listing.findById(id);
+    res.render("listings/edit.ejs",{listing});
+}))
+
+//update route
+app.put("/listings/:id",validateListing,wrapAsync(async (req,res)=>
+{
+    let {id}=req.params;
+    await Listing.findByIdAndUpdate(id,{...req.body.listing});
+    res.redirect(`/listings/${id}`);
+}))
+
+//delete route
+app.delete("/listings/:id/delete",wrapAsync(async (req,res)=>
+{
+    let{id}=req.params;
+    await Listing.findByIdAndDelete(id);
+    res.redirect("/listings");
+}))
+
+//handling wrong route error
+app.all("*",(req,res,next)=>
+{
+    next(new ExpressError(404,"Page Not Found!"));
+})
+app.use((err,req,res,next)=>
+{
+    let{status=500,message="somthing went wrong"}=err;
+    res.status(status).render("error.ejs",{err});
+})
+app.listen(8080,()=>
+{
+    console.log("server listening");
+})
---
+++ models/listing.js
@@ -0,0 +1,26 @@
+const mongoose=require("mongoose");
+const Schema=mongoose.Schema;
+
+const listingSchema=new Schema(
+    {
+        title:
+        {
+            type:String,
+            required:true
+        },
+        description:String,
+        image:
+        {
+            type:String,
+            default:"https://images.unsplash.com/photo-1509233725247-49e657c54213?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Nnx8YmVhY2h8ZW58MHx8MHx8fDA%3D",
+            set:(v)=> v==="" ? "https://images.unsplash.com/photo-1509233725247-49e657c54213?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Nnx8YmVhY2h8ZW58MHx8MHx8fDA%3D":v,
+        },
+        price:Number,
+        location:String,
+        country:String
+    }
+);
+
+const Listing=mongoose.model("Listing",listingSchema);
+
+module.exports=Listing;

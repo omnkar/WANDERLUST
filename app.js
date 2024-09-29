92dbea8a61e6e0295879928c338a6977b69771c6
Onkar <onkarmuthe.2016@gmail.com>
2024-09-29 15:30:36 +0530

    updated

--- app.js
+++ app.js
@@ -7,9 +7,9 @@ const methodOverride=require("method-override");
 const ejsMate=require("ejs-mate");
 const wrapAsync=require("./utils/wrapAsync.js");
 const ExpressError=require("./utils/ExpressError.js");
-const {listingSchema}=require("./schema.js");
+const {listingSchema,reviewSchema}=require("./schema.js");
 const Review=require("./models/review.js");
-const {reviewSchema}=require("./schema.js");
+const listings=require("./routes/listing.js");
 
 app.set("view engine","ejs");
 app.set("views",path.join(__dirname,"views"));
@@ -38,9 +38,10 @@ app.get("/",(req,res)=>
     res.send("hei i am rooot");
 });
 
-const validateListing=(req,res,next)=>
-{
-    let {error}=listingSchema.validate(req.body);
+
+const validateReview=(req,res,next)=>
+    {
+        let {error}=reviewSchema.validate(req.body);
         if(error)
         {
             let errMsg=error.details.map((el)=>el.message).join(",");
@@ -49,75 +50,11 @@ const validateListing=(req,res,next)=>
         else{
             next();
         }
-
-}
-
-const validateReview=(req,res,next)=>
-{
-    let {error}=reviewSchema.validate(req.body);
-    if(error)
-    {
-        let errMsg=error.details.map((el)=>el.message).join(",");
-        throw new ExpressError(400,errMsg);
     }
-    else{
-        next();
-    }
-}
-//index route
-app.get("/listings",wrapAsync(async (req,res)=>
-{
-    const allListing=await Listing.find({});
-    res.render("listings/index.ejs",{allListing});
-}))
 
-//new route
-app.get("/listings/new",(req,res)=>
-    {
-        res.render("listings/new.ejs");
-    })
+//middleware for restructuring of listings
+app.use("/listings",listings);
 
-//show route
-app.get("/listings/:id",wrapAsync(async(req,res)=>
-{
-    let{id}=req.params;
-    const listing=await Listing.findById(id).populate("reviews");
-    res.render("listings/show.ejs",{listing});
-
-}))
-//create route
-app.post("/listings",validateListing,wrapAsync(async(req,res,next)=>
-{
-        
-        const newlist=new Listing(req.body.listing);
-        await newlist.save();
-        res.redirect("/listings");
-})
-);
-
-//edit route
-app.get("/listings/:id/edit",wrapAsync(async (req,res)=>
-{
-    let {id}=req.params;
-    const listing=await Listing.findById(id);
-    res.render("listings/edit.ejs",{listing});
-}))
-
-//update route
-app.put("/listings/:id",validateListing,wrapAsync(async (req,res)=>
-{
-    let {id}=req.params;
-    await Listing.findByIdAndUpdate(id,{...req.body.listing});
-    res.redirect(`/listings/${id}`);
-}))
-
-//delete route
-app.delete("/listings/:id/delete",wrapAsync(async (req,res)=>
-{
-    let{id}=req.params;
-    await Listing.findByIdAndDelete(id);
-    res.redirect("/listings");
-}))
 //reviews route
 app.post("/listings/:id/reviews",validateReview,wrapAsync(async(req,res)=>
     {
